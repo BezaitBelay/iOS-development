a3477a4c870e3b4be487c14cfae4ff82fd6420d9
Bezait <beizi_97@abv.bg>
2020-04-08 14:09:15 +0300

    first task commit

---
+++ Task1/Task1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task1
+//
+//  Created by Dynamo Software on 5.04.20.
+//  Copyright © 2020 Dynamo Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
